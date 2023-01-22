71e3786d1ac540a5bff7deb148753d6128fa3d01
auro736 <auroragensale@gmail.com>
2023-01-22 18:42:40 +0100

    translation file works

--- Experiments/Utils/from_en_to_fr.py
+++ Experiments/Utils/from_en_to_fr.py
@@ -8,7 +8,7 @@ def eng_to_fr(file_path, split):
 
     x = open(file_path).readlines()
     en_fr_translator = pipeline("translation_en_to_fr")
-
+    
     for line in tqdm(x):
 
         instance = json.loads(line)
@@ -31,9 +31,9 @@ def eng_to_fr(file_path, split):
         else:
             dict_["answerKey"] = "A"
 
-        #saving_fp = f"/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/FR/{split.lower}_FR.jsonl"
+        saving_fp = f"/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/FR/{split.lower()}_FR.jsonl"
         
-        saving_fp = f"/content/DNLP_project/data/qasc/FR/{split.lower}_FR.jsonl"
+        #saving_fp = f"/content/DNLP_project/data/qasc/FR/{split.lower}_FR.jsonl"
 
         with open(saving_fp, "a", encoding='utf8') as outfile:
             #outfile.write(json_object)
@@ -42,21 +42,19 @@ def eng_to_fr(file_path, split):
 
 
 
-        
-
 if __name__ == "__main__":
 
-    # json_path_train = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/train.jsonl"
-    # json_path_valid = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/dev.jsonl"
-    # json_path_test = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/test.jsonl"
+    json_path_train = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/train.jsonl"
+    json_path_valid = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/dev.jsonl"
+    json_path_test = "/mnt/c/Users/auror/Desktop/NUOVA REPO PROGETTO DNLP/DNLP_project/data/qasc/test.jsonl"
 
-    json_path_train = "/content/DNLP_project/data/qasc/train.jsonl"
-    json_path_valid = "/content/DNLP_project/data/qasc/dev.jsonl"
-    json_path_test = "/content/DNLP_project/data/qasc/test.jsonl"
+    # json_path_train = "/content/DNLP_project/data/qasc/train.jsonl"
+    # json_path_valid = "/content/DNLP_project/data/qasc/dev.jsonl"
+    # json_path_test = "/content/DNLP_project/data/qasc/test.jsonl"
 
-    eng_to_fr(json_path_train,split = "Train")
+    #eng_to_fr(json_path_train,split = "Train")
     eng_to_fr(json_path_valid,split = "Dev")
-    eng_to_fr(json_path_test,split = "Test")
+    #eng_to_fr(json_path_test,split = "Test")
 
 
        
